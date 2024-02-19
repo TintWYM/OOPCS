e4a6233d678a486758a65bdbf1117b7519c0bd5f
Andr3wCh3n <tintwaiyanmin.sg@gmail.com>
2024-02-19 15:52:06 +0800

    first commit

---
+++ Workshop1/Workshop1/LED.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Workshop1
+{
+	public class LED
+	{
+        private bool isOn;
+        public string color;
+        private int tracker;
+        private static readonly string[] colors = { "Red", "Green", "Blue" };
+
+        public LED()
+        {
+            isOn = false;
+            this.color = "";
+            this.tracker = -1;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public bool isLEDOn()
+        {
+            return isOn;
+        }
+
+        public void isTurnOn() {
+            if (!isLEDOn()) {
+                isOn = true;
+                tracker = IncrementTracker();
+                color = colors[tracker];
+            }
+        }
+
+        public void isTurnOff()
+        {
+            if (isLEDOn())
+            {
+                isOn = false;
+                color = "";
+            }
+        }
+
+        private int IncrementTracker()
+        {
+            tracker = (tracker + 1) % colors.Length;
+            return tracker;
+        }
+    }
+}
+
---
+++ Workshop1/Workshop1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Workshop1 { 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int choice = 0;
+            LED led = new LED();
+
+            while (true) {
+                DisplayMenu();
+                choice = getChoice();
+                setChoice(choice, led);
+            }
+        }
+
+        private static void DisplayMenu() {
+            Console.WriteLine("Please enter your choice: ");
+            Console.WriteLine("1. Turn on light");
+            Console.WriteLine("2. Turn off light");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("My choice: ");
+        }
+
+        private static int getChoice() {
+            int choice = Convert.ToInt16(Console.ReadLine());
+            return choice;
+        }
+
+        private static void setChoice(int choice, LED led)
+        {
+            switch (choice)
+            {
+                case 1:
+                    led.isTurnOn();
+                    PrintStatus(led);
+                    break;
+                case 2:
+                    led.isTurnOff();
+                    PrintStatus(led);
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    System.Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static void PrintStatus(LED led) {
+            Console.WriteLine("\n** The LED is now " + (led.isLEDOn() ? "ON and its color is " + led.getColor() : "OFF") + "**\n");
+        }
+    }
+}
